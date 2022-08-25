425dc8208db1b20ae373e1f5a6a217773a4310d3
nklopstock611 <77993013+nklopstock611@users.noreply.github.com>
2022-08-25 16:33:09 -0500

    cambios buzon

--- caso-1/src/Buzon.java
+++ caso-1/src/Buzon.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 
 public class Buzon {
@@ -8,6 +9,33 @@ public class Buzon {
     public Buzon(int size) {
         this.n = size;
         this.arrBuzon = new ArrayList<String>();
+    }
+    
+    public synchronized void almacenar(String s, int transf, int nivel) {
+    	while (arrBuzon.size() == 0) {
+    		try {
+    			wait();
+    		} catch (InterruptedException e) {
+    			e.printStackTrace();
+    		}
+    	}
+    	s += Integer.toString(transf) + "" + Integer.toString(nivel);
+    	arrBuzon.add(s);
+    	notify();
+    }
+    
+    public synchronized String retirar() {
+        while (arrBuzon.size() == 0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        String s = arrBuzon.remove(0);
+        notify();
+        
+        return s;
     }
     
 }
